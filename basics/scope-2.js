c93ce9bd877571c52e1590a37c31b32410badc46
shivaniJEunimart <shivani.jaiswal@eunimart.com>
2020-07-23 16:19:42 +0530

    first commit

---
+++ basics/scope-2.js
@@ -0,0 +1,20 @@
+//Global (name)
+    //local(name)
+        //local
+    //local
+
+let name = 'Shivani'
+
+if(true){
+    let name = 'mike'
+
+    if(true){
+        name = 'jen'
+        console.log(name)
+
+    }
+}
+
+if(true){
+    console.log(name)
+}
---
+++ basics/templet-string.js
@@ -0,0 +1,3 @@
+let name = 'shivani'
+let age = 20
+console.log(`My name us ${name}!.I am ${age} old.`)
